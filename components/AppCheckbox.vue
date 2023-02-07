b7c621815229b742736127f4bcf663819d44de24
bogdan12893 <bogdan1281993@gmail.com>
2023-02-07 15:42:30 +0200

    checkbox

---
+++ components/AppCheckbox.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { useField } from 'vee-validate';
+
+const props = defineProps({
+  modelValue: {
+    type: null,
+    default: false,
+  },
+  name: {
+    type: String,
+    default: '',
+  },
+  value: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const { checked, handleChange, errorMessage } = useField(props.name, undefined, {
+  type: 'checkbox',
+  valueProp: props.value,
+  uncheckedValue: false,
+  checkedValue: true,
+});
+</script>
+
+<template lang="pug">
+.app-checkbox
+  label(:for="name")
+    input(
+      type="checkbox"
+      :name="name"
+      :id="name"
+      :value="value"
+      @input="handleChange($event.target.value)"
+    )
+    span.ml-2 Please check this box {{ checked }}
+  .error {{ errorMessage }}
+</template>
+
+<style lang="scss" scoped></style>
+
+<style lang="scss" scoped>
+.app-checkbox {
+  @apply my-3;
+
+  input,
+  textarea {
+    @apply bg-green-100 py-3 px-4 rounded-md border border-green-500;
+  }
+
+  .error {
+    @apply text-red-600 text-xs;
+  }
+}
+</style>
--- pages/simple-form.vue
+++ pages/simple-form.vue
@@ -2,11 +2,13 @@
 import { useForm } from 'vee-validate';
 const email = ref('');
 const password = ref('');
+const checkbox = ref(false);
 
 const formSchema = computed(() => {
   return {
     password: 'required',
     email: 'required|email',
+    checkbox: 'required',
   };
 });
 
@@ -32,5 +34,6 @@ const handleFormSubmit = async () => {
   form
     AppInput(inputType="text" inputName="email" placeholder="email" v-model:modelValue="email")
     AppInput(inputType="password" inputName="password" placeholder="password" v-model:modelValue="password")
+    AppCheckbox(name="checkbox" v-model:modelValue="checkbox" :value="true")
   button(@click.prevent="handleFormSubmit" type="submit") Submit
 </template>
