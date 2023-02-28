e17d3503a56bd746c44e8ff565cda16bcc3dc668
bogdan12893 <bogdan1281993@gmail.com>
2023-02-28 08:10:48 +0200

    implement scroll to error validation

--- pages/simple-form.vue
+++ pages/simple-form.vue
@@ -9,18 +9,28 @@ const form = reactive({
 
 const formSchema = computed(() => {
   return {
-    password: 'required',
     email: 'required|email',
+    password: 'required',
     terms: 'required',
   };
 });
 
-const { validate } = useForm({
+const { validate, errors } = useForm({
   validationSchema: formSchema,
 });
 
+const scrollToValidation = (errorName) => {
+  const el = document.querySelector(`[name="${errorName}"]`);
+  if (el) {
+    el.scrollIntoView({ block: 'start', behavior: 'smooth' });
+  }
+};
+
 const handleFormSubmit = async () => {
   const validator = await validate();
+
+  const firstErrorFieldName = Object.keys(errors.value)[0];
+  scrollToValidation(firstErrorFieldName);
 
   if (!validator.valid) {
     return;
@@ -37,6 +47,8 @@ const handleFormSubmit = async () => {
     AppInput(inputType="text" inputName="email" placeholder="email" v-model="form.email")
     AppInput(inputType="password" inputName="password" placeholder="password" v-model="form.password")
     AppCheckbox(name="terms" inputId="terms" v-model="form.terms" :value="true" text="Terms check!")
+    div(class="h-[1000px] w-1/2 bg-gray-100 p-8 my-4")
+      p Scroll to invalid validation check
   button(@click.prevent="handleFormSubmit" type="submit") Submit
 
   pre {{ form }}
